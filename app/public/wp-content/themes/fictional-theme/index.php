d1e3f154ecdcc736eccec8009324371417cd1a9a
viseth89 <viseth89@gmail.com>
2019-02-05 22:41:49 -0600

    working on footer

--- app/public/wp-content/themes/fictional-theme/index.php
+++ app/public/wp-content/themes/fictional-theme/index.php
@@ -1,1 +1,17 @@
-This is our completely amazing custom theme
+<?php get_header();
+
+  while(have_posts()) {
+    the_post(); ?>
+
+    <h2><a href="<?php the_permalink(); ?>">  <?php the_title(); ?>      </a></h2>
+    <p><?php the_content(); ?></p>
+    <hr>
+
+  <?php }
+
+
+    get_footer();
+?>
+
+
+blah
